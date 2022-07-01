8492059eea036339063bb36443cab1e68634bd64
Majid <mj_azani@hotmail.com>
2022-07-01 10:59:36 +0430

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -6,5 +6,5 @@ curl -o- https://get.docker.com | bash +x
 # Install docker composer
 curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
-ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
 echo "docker & docker-compse installed successfully"
